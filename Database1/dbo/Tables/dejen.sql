542f89679a0dac734fb03132f0346456a3953fae
dejen <dejenfasil@gmail.com>
2019-11-30 15:08:54 -0500

    Add three new tables

---
+++ Database1/dbo/Tables/dejen.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[dejen] (
+    [id]   INT          NOT NULL,
+    [name] VARCHAR (20) NULL,
+    [flag] BIT          DEFAULT ((0)) NULL,
+    PRIMARY KEY CLUSTERED ([id] ASC)
+);
+
